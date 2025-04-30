fd88fbc5ffe903a7314c0ebc7ede9cdf6ea1c67c
Noman Atique <noman.atique@keyin.com>
2025-04-30 11:30:58 -0400

    april30JS

--- JavaScript/Week1/first.js
+++ JavaScript/Week1/first.js
@@ -1,8 +1,115 @@
-console.log("hello from first js file");
+// console.log("hello from first js file");
 
-let age = 20;
-if (age >= 20) {
-  console.log("you are above 20");
-} else {
-  console.log("You are underage");
-}
+// let age = 20;
+// if (age >= 20) {
+//   consoe.log("you are above 20");
+// } else {
+//   console.log("You are underage");
+// }
+
+// console.log("ending here");
+
+// Syntax of JavaScript language!
+
+// let customerCount = 10;
+// let CustomerCount = 12;
+
+// camelCase, dateOfBirth, RECOMMENDED....
+// PascalCase, DateOfBirth,
+// snake_case, date_of_birth,
+// kebab-case, data-of-birth (CSS ....text-decoration:... background-color)
+// console.log("hello");
+// console.log("hello again");
+
+/* 
+
+Name: Noman Atique
+Assingment: QAP1
+Date: April 30,2025 */
+
+// let x = 10; //statement has a statement terminator ;
+// let y = 20;
+// {
+//   console.log("something");
+//   console.log("hello");
+//   let names = ["Alic", "Barbara"];
+// }
+
+// let age = 20;
+// if (age > 20) {
+//   console.log("twenty");
+//   console.log("Good");
+// }
+
+// A block starts with {  and ends  }
+
+// function foo() {
+//   //body of the function
+// }
+// int x; string y; boolean x;
+
+// let x;
+// x = 10;
+// console.log(x, typeof x);
+
+// x = "Alison";
+// console.log(x, typeof x);
+
+// x = true;
+// console.log(x, typeof x);
+
+// var x;
+
+// ES5 ES6 Advanced versions of JS...
+
+// let and const..
+
+// {
+//   const x = 10;
+//   //   x = 10;
+//   console.log(x);
+//   //   x = x + 10;
+// }
+// console.log(x);
+
+// let has a "BLOCK level scope", const has the same scope as let.. but we cannot re assign a const value...
+// var has a "function level scope"
+
+// let lastName; //NO SPACE BETWEEN VARIABLE/FUNCITON names....
+
+// let age; //Hoisting...
+
+// {
+//   age = 20;
+//   console.log(age);
+// }
+
+// console.log(age);
+
+// let x = "10.2";
+// console.log(x, typeof x);
+
+// " string",   'string', `string`
+
+// let foo = function () {
+//   console.log("foo function");
+// };
+
+// console.log(typeof foo);
+
+// let numbers = [1, 2, 3];
+// console.log(typeof numbers);
+
+// let numbers = new Date();
+// console.log(typeof numbers);
+
+// let x = 10;
+// x += 20;
+// console.log(x);
+// Increment/Decrement... Pre-increment/ Post-increment!
+
+let x = 10;
+console.log(x++); //10 ...11
+console.log(--x); // 10...10
+console.log(x--); // 10... 9
+console.log(++x); // 10... 10
