0aa32ce424d38d86d2b20dba3ada7fd501c78998
Noman Atique <noman.atique@keyin.com>
2025-05-07 11:24:44 -0400

    may7_js

--- JavaScript/Week2/script.js
+++ JavaScript/Week2/script.js
@@ -94,11 +94,95 @@
 // Parameters and ...
 // ... "rest" parameter, it allows us to specify all final argument to a function..by naming ourselves..
 
-function sum(a, b, c, ...numbers) {
-  let total = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    total += numbers[i];
-  }
-  return total;
+// function sum(...numbers) {
+//   let total = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     total += numbers[i];
+//   }
+//   return total;
+// }
+// console.log(sum(1, 2, 3, 4, 5));
+// true and false... Boolean values
+// truthy and falsy...
+
+// Dealing with Optional and Missing Arguments
+
+// function updateScore(currentScore, value, bonus = 1) {
+//   // 1. using if else
+//   // if (bonus) {
+//   //   return currentScore + value * bonus;
+//   // } else {
+//   //   return currentScore + value;
+//   // }
+//   // 2. using ternary operator
+//   // return bonus ? currentScore + value * bonus : currentScore + value;
+//   // 3. usign logical OR operator
+//   // bonus = bonus || 1;
+//   // return currentScore + value * bonus;
+//   // 4. Using shortcut for number 3...
+//   // return currentScore + value * (bonus || 1);
+//   // 5. default parameters..
+
+//   return currentScore + value * bonus;
+// }
+
+// console.log(updateScore(10, 3));
+// console.log(updateScore(10, 3, 2));
+
+// Binding functions with variables...
+
+// function greeting(greeting, name) {
+//   return greeting + " " + name;
+// }
+
+// var sayHi = greeting; //bind a reference to greeting to sayHi
+
+// console.log(greeting("Hello", "Steven"));
+// console.log(sayHi("Hi", "Christina"));
+
+// Built-in/Pre-defned/Global Functions
+
+// parseInt()
+// parseFloat()
+// let age = "20years";
+// let height = "5.6feet";
+
+// console.log(parseInt(age));
+// console.log(parseFloat(height));
+
+// inNaN()
+
+// console.log(isNaN(5));
+// console.log(isNaN("a5"));
+
+// console.*
+
+// Math. abs(), max(), min(), random(), round()
+// Date.now()
+// JSON.parse(), JSON.stringify()...Parse converts JSON data to normal JavaScript.. Stringify() converts normal javascript to JSON..
+
+// Introduction to Objects and Object Oriented Programming
+// object is a collection of data and methods.. together...
+
+let str = "The best sound in. this world, is your name";
+
+function addPeriod(s) {
+  // if (s.endsWith(".")) {
+  //   return s;
+  // } else {
+  //   return s + ".";
+  // }
+
+  // return s.endsWith(".") ? s : `${s}.`;
+
+  // return s.includes(".") ? s : `${s}.`; //not a good idea..
+
+  let lastPos = s.length - 1;
+
+  // return s.charAt(lastPos) === "." ? s : `${s}.`;
+
+  return s.lastIndexOf(".") === lastPos ? s : `${s}.`;
 }
-console.log(sum(1, 2, 3, 4, 5));
+
+console.log(addPeriod("hello."));
+console.log(addPeriod(str));
