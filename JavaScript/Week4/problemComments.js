55c765fbc8b4b770ba7c799b4c5e7086d44d0d4a
Noman Atique <noman.atique@keyin.com>
2025-05-26 08:20:16 -0400

    may27

--- JavaScript/Week4/problemComments.js
+++ JavaScript/Week4/problemComments.js
@@ -63,36 +63,77 @@ function extractAreaCode(phoneNumber) {
 }
 
 function normalizeHeight(height) {
+  // height is already in inches...
   if (height.endsWith("inches")) {
     return height;
   }
-
+  // Get the height in cm as a Number...
   let cm = parseFloat(height);
-
+  // I get the number attached to the height...
   let inches = cm * 0.39;
 
   return `${Math.round(inches)} inches`;
 }
 
 function rowToFields(row) {
+  //split on with our without space on either side..
+
   let fields = row.split(/\s*,\s*/);
 
+  //   Remove extra spaces from name field
+  //   fields[0] ID, fields[1] Name, fields[2] phone , fields[3] height
+  // fields[1] = fields[1].replace(/\s+/, " ");//replace this line with function call
+
   fields[1] = removeExtraWhiteSpace(fields[1]);
+
+  //   Extract area code from phone number!
+  // let phoneNumber = fields[2];
+  // let matches = phoneNumber.match(/(\(?\d{3}\)?)?[\s\-]*\d{3}[\s\-]*\d{4}/);
+  // if (matches) {
+  //   // fields[2] = matches[1] + "1234456";
+
+  //   fields[2] = matches[1];
+
+  //   fields[2] = fields[2].trim();
+  //   fields[2] = `${fields[2]}- ${fields[2]} - ${fields[2]}`;
+  //   // Now you need to make this phone consistant for all records??
+  // }
+  // return fields;
 
   fields[2] = extractAreaCode(fields[2]);
 
   fields[3] = normalizeHeight(fields[3]);
 
+  // to make these fields into rows...
   return fields.join(",");
 }
 
 function processCSV(csv) {
+  // do all the required steps
+
   //   Step1: break the CSV into rows
 
   let rows = splitIntoRows(csv);
 
+  //   Step 2: split all rows into array of fields..
+
+  //   let data = [];
+  //   for (let i = 0; i < rows.length; i++) {
+  //     let row = rows[i];
+  //     let fields = rowToFields(row);
+  //     console.log(fields);
+  //     data.push(fields);
+  //   }
+
+  //   for (let row of rows) {
+  //     let fields = rowToFields(row);
+  //     data.push(fields);
+  //   }
+
   let data = rows.map((row) => rowToFields(row));
 
+  // console.log(data.join("\n"));
+  //   console.log(rows);
   return data.join("\n");
 }
 
@@ -101,3 +142,7 @@ console.log(csvData);
 console.log("-----------------------");
 var processed = processCSV(csvData);
 console.log(processed);
+
+// Please practice this question, and the practice questions of Monday, because QAP2 is going to be hard, if YOU DONT DO IT...
+// Comments: We were not ready for this type of QAP2?
+// Comments: Class material did not cover a lot of what is give QAP2...
