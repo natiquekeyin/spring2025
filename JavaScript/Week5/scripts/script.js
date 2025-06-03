12aa398b52f6d94d28385240e97ed3da26f03291
Noman Atique <noman.atique@keyin.com>
2025-06-03 10:48:33 -0400

    week5JS

--- JavaScript/Week5/scripts/script.js
+++ JavaScript/Week5/scripts/script.js
@@ -101,35 +101,85 @@
 
 // "Constructor" function ..that can be used to construct objects...
 
-function User(f, l, a) {
-  this.firstName = f;
-  this.lastName = l;
-  this.age = a;
-  this.isLoggedIn = true;
-  this.hobbies = ["Reading", "Cycling"];
-  this.pref = { color: "Red", model: "Volvo" };
-  this.login = function () {
-    this.isLoggedIn = true;
-  };
-  //   this.logout = function () {
-  //     this.isLoggedIn = false;
-  //   };
-}
-
-// User.prototype.login = function () {
+// function User(f, l, a) {
+//   this.firstName = f;
+//   this.lastName = l;
+//   this.age = a;
 //   this.isLoggedIn = true;
+//   this.hobbies = ["Reading", "Cycling"];
+//   this.pref = { color: "Red", model: "Volvo" };
+//   this.login = function () {
+//     this.isLoggedIn = true;
+//   };
+//   //   this.logout = function () {
+//   //     this.isLoggedIn = false;
+//   //   };
+// }
+
+// // User.prototype.login = function () {
+// //   this.isLoggedIn = true;
+// // };
+
+// User.prototype.logout = function () {
+//   this.isLoggedIn = false;
 // };
 
-User.prototype.logout = function () {
-  this.isLoggedIn = false;
-};
+// let user1 = new User("Alan", "Smith", 20); //"this" will refer to this object
+// let user2 = new User("Bob", "Smith", 22); //"this" will refer to this object
+// let user3 = new User("Christina", "Lee", 21); //"this" will refer to this object...
 
-let user1 = new User("Alan", "Smith", 20); //"this" will refer to this object
-let user2 = new User("Bob", "Smith", 22); //"this" will refer to this object
-let user3 = new User("Christina", "Lee", 21); //"this" will refer to this object...
-
-console.log(user1);
-user1.logout();
-console.log(user1);
+// console.log(user1);
 // user1.logout();
 // console.log(user1);
+// // user1.logout();
+// // console.log(user1);
+
+// Starting with cap means it's constructor funciton...
+
+/* 
+
+Write a constructor function "Person", that takes name and age as parameters, and creates object with these two fields only.
+Then write a function addPerson(name,age) that receives two parameters and by using those two parameters it creates a new object of Person method, and pushes the newly create object into an array called people = [];
+*/
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// let people = [];
+// function addPerson(n, a) {
+//   let user = new Person(n, a);
+//   people.push(user);
+// }
+
+// addPerson("Alan Smith", 23);
+// addPerson("Bob Smith", 19);
+// addPerson("Christina Lee", 20);
+// console.log(people);
+
+/* 
+
+Write a constructor function "Car" that has following properties:
+    1. make(string)
+    2. model(string)
+    3. year(number)
+
+    Add a method getCarInfo() that returns a string combining all the properties
+    For example:
+
+    let car1 = new Car("Toyota", "Corolla",2019);
+    console.log(car1.getCarInfo()); .....//outputs "Toyota Corolla, 2019"
+*/
+
+function Car(mk, md, yr) {
+  this.make = mk;
+  this.model = md;
+  this.year = yr;
+
+  this.getCarInfo = function () {
+    return `The make is ${this.make}: The model is ${this.model} & the year is ${this.year}`;
+  };
+}
+
+let car1 = new Car("Honda", "Odyssey", 2024);
+console.log(car1.getCarInfo());
