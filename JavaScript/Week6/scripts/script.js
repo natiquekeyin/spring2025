ea7566812d2ecfa11b07dacb64dd3e11ef2b625c
Noman Atique <noman.atique@keyin.com>
2025-06-03 11:17:42 -0400

    june3

---
+++ JavaScript/Week6/scripts/script.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", function () {
+  let posts = [
+    { title: "Post1", author: "Author1", body: "This is post1 body" },
+    { title: "Post2", author: "Author2", body: "This is post2 body" },
+    { title: "Post3", author: "Author3", body: "This is post3 body" },
+  ];
+
+  function createPost(post, cb) {
+    setTimeout(function () {
+      posts.push(post);
+      //add the given post... tothe posts array on the top
+      cb(); //this cb will be called once all functionality is completed...
+    }, 3000);
+  }
+
+  function getPosts() {
+    setTimeout(function () {
+      posts.forEach(function (post) {
+        // document.querySelector("#output").innerText += post.title;
+
+        document.querySelector(
+          "#output"
+        ).innerHTML += `Title: ${post.title} <br/>
+        Author: ${post.author} <br/>
+        Body: ${post.body}<br/>----------------<br/>
+        `;
+      });
+    }, 1000);
+  }
+
+  createPost(
+    { title: "Post4", author: "Author4", body: "This is post4 body" },
+    getPosts
+  );
+});
+
+// 1. Use callback functions...
