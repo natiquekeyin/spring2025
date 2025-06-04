abccbe40a956354b6a3c689cff4297e808208792
Noman Atique <noman.atique@keyin.com>
2025-06-04 08:27:37 -0400

    june4

---
+++ FrontEnd/Week6/scripts/library.js
@@ -0,0 +1,49 @@
+window.addEventListener("DOMContentLoaded", function () {
+  let form = this.document.querySelector("#form1");
+
+  form.addEventListener("submit", handleSubmit);
+
+  function handleSubmit(evt) {
+    let title = document.querySelector("#title").value;
+    let isbn = document.querySelector("#isbn").value;
+    let author = document.querySelector("#author").value;
+
+    addBookToList(title, isbn, author);
+
+    evt.preventDefault();
+  }
+
+  function addBookToList(t, i, a) {
+    // this function will actually create the row in the table below form...
+
+    if (t === "" || i === "" || a === "") {
+      showAlert("No field shoud be empty", "error");
+    } else {
+      let row = document.createElement("tr");
+
+      row.innerHTML = `<td>${t}</td><td>${i}</td><td>${a}</td><td class="delete">X</td>`;
+
+      document.querySelector("#list").appendChild(row);
+      showAlert("Book Succesfully added", "success");
+      clearFields();
+    }
+  }
+
+  function clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#isbn").value = "";
+    document.querySelector("#author").value = "";
+  }
+
+  function showAlert(m, c) {
+    let p = document.createElement("p");
+    p.innerText = m;
+    p.className = c;
+    p.id = "box";
+    document.querySelector("#notify").appendChild(p);
+
+    setTimeout(function () {
+      document.querySelector("#box").remove();
+    }, 3000);
+  }
+});
