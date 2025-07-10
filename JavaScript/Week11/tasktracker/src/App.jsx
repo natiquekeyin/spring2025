b39b4c428ee43365d3a10d0ce4dc8e1979189240
Noman Atique <noman.atique@keyin.com>
2025-07-10 11:27:06 -0400

    july10

--- JavaScript/Week11/tasktracker/src/App.jsx
+++ JavaScript/Week11/tasktracker/src/App.jsx
@@ -1,33 +1,35 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
-import UEffect from "./components/Temp/UEffect";
+
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctor's appointment",
-      day: "July 10,2025 at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Parent teacher's meeting",
-      day: "July 17,2025 at 1:30pm",
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: "GYM",
-      day: "August 6,2025 at 2:30pm",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
   // Delete task
 
-  const deleteTask = (id) => {
+  // bring data from json server
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch("http://localhost:8000/tasks");
+      const data = await res.json();
+      // console.log(data);
+      return data;
+    };
+
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
@@ -44,11 +46,20 @@ function App() {
 
   // To add a new task
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+    const data = await res.json();
+
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newTask = { id, ...task };
+    setTasks([...tasks, data]);
   };
 
   return (
@@ -58,13 +69,12 @@ function App() {
         onAdd={() => setShowAddTask(!showAddTask)}
         showAdd={showAddTask}
       />
-      {/* {showAddTask && <AddTask onAdd={addTask} />}
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No tasks to show!!!"
-      )} */}
-      <UEffect />
+      )}
     </div>
   );
 }
