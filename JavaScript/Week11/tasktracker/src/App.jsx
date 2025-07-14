788168e9e28540c5252c4f5bb72c922b40ef2763
Noman Atique <noman.atique@keyin.com>
2025-07-14 08:24:29 -0400

    july14

--- JavaScript/Week11/tasktracker/src/App.jsx
+++ JavaScript/Week11/tasktracker/src/App.jsx
@@ -2,11 +2,24 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import USArray from "./components/Temp/USArray";
+import TaskDetails from "./components/TaskDetails";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
   // Delete task
+
+  //Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:8000/tasks/${id}`);
+
+    const data = await res.json();
+    return data;
+  };
 
   // bring data from json server
 
@@ -35,8 +48,19 @@ function App() {
 
   // Toggle Reminder
 
-  const toggleReminder = (id) => {
-    console.log("toggle working");
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
@@ -63,20 +87,45 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        title="Task Tracker"
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No tasks to show!!!"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          title="Task Tracker"
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No tasks to show!!!"
+                )}
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+
+          <Route path="/usarray" element={<USArray />} />
+
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// The API calls: GET,POST,PUT, and DELETE...
---
+++ JavaScript/Week11/tasktracker/src/components/TaskDetails.jsx
@@ -0,0 +1,6 @@
+const TaskDetails = () => {
+  return <div>TaskDetails</div>;
+};
+export default TaskDetails;
+
+// Let's create TaskDetails in tomorrow session...
